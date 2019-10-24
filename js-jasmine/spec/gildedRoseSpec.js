9111050aee02cfeeb9f35e1f87c77e2298d3babf
mackacavs <mackenziegdixon@yahoo.com>
2019-10-24 17:47:14 +0100

    Have added Completed Test Suite

--- js-jasmine/spec/gildedRoseSpec.js
+++ js-jasmine/spec/gildedRoseSpec.js
@@ -10,31 +10,16 @@ describe("Gilded Rose", function () {
 
   describe("#updateQuality", function () {
 
-    it("should reduce the quality of the item by 1 when updateQuality is called as long as the name isn't 'aged brie', 'Backstage passes to a TAFKAL80ETC concert' or 'Sulfuras, Hand of Ragnaros' and the quality of the item doesn't become negative",
+    it("shouldn't decrease the quality of the item if the item's name is 'Sulfuras, Hand of Ragnaros'",
       function () {
-        let chocolate = new ItemObj('chocolate', 5, 10)
-        const shop = new Shop([chocolate]);
+        let sulfuras = new ItemObj('Sulfuras, Hand of Ragnaros', 5, 80)
+        const shop = new Shop([sulfuras]);
         shop.updateQuality()
-        expect(chocolate.quality).toEqual(9);
+        expect(sulfuras.quality).toEqual(80);
       });
 
-    it("should not reduce the quality of the itme further if the item's quality is already 0",
-      function () {
-        let chocolate = new ItemObj('chocolate', 5, 0)
-        const shop = new Shop([chocolate]);
-        shop.updateQuality()
-        expect(chocolate.quality).toEqual(0);
-      });
 
-    it("should increase the quality of the itme by 1 if the item's name is already 'Aged Brie'",
-      function () {
-        let aged_brie = new ItemObj('Aged Brie', 5, 10)
-        const shop = new Shop([aged_brie]);
-        shop.updateQuality()
-        expect(aged_brie.quality).toEqual(11);
-      });
-
-    it("shouldn't increase the quality of the itme by 1 if the item's quality is already 50",
+    it("shouldn't increase the quality of the item if the item's quality is already 50",
       function () {
         let sweets = new ItemObj('sweets', 5, 50)
         const shop = new Shop([sweets]);
@@ -42,32 +27,84 @@ describe("Gilded Rose", function () {
         expect(sweets.quality).toEqual(50);
       });
 
-    it("shouldn't decrease the quality of the item if the item's name is 'Sulfuras, Hand of Ragnaros'",
+    it("should not reduce the quality of the item further if the item's quality is already 0",
       function () {
-        let sulfuras = new ItemObj('Sulfuras, Hand of Ragnaros', 5, 10)
-        const shop = new Shop([sulfuras]);
+        let chocolate = new ItemObj('chocolate', 5, 0)
+        const shop = new Shop([chocolate]);
         shop.updateQuality()
-        expect(sulfuras.quality).toEqual(10);
+        expect(chocolate.quality).toEqual(0);
       });
 
-
-  })
-
-  describe("#checkItemNotSpecial", function () {
-    let shop;
-
-    beforeEach(function () {
-      shop = new Shop();
-    });
-
-    it("return true if the argument entered is noy 'Aged Brie', 'Backstage passes to a TAFKAL80ETC concert' or 'Sulfuras, Hand of Ragnaros'",
+    it("should decrease the quality of the item by 2 if the item's sellIn is 0",
       function () {
-        expect(shop.checkItemNotSpecial('chocolate')).toEqual(true);
+        let sweets = new ItemObj('sweets', 0, 10)
+        const shop = new Shop([sweets]);
+        shop.updateQuality()
+        expect(sweets.quality).toEqual(8);
       });
 
-    it("return false if the argument entered is 'Aged Brie', 'Backstage passes to a TAFKAL80ETC concert' or 'Sulfuras, Hand of Ragnaros'",
+    it("should increase the quality of the item by 1 if the item's name is 'Aged Brie' and the sellIn date is yet to be reached",
       function () {
-        expect(shop.checkItemNotSpecial('Aged Brie')).toEqual(false);
+        let aged_brie = new ItemObj('Aged Brie', 11, 10)
+        const shop = new Shop([aged_brie]);
+        shop.updateQuality()
+        expect(aged_brie.quality).toEqual(11);
+      });
+
+    it("should increase the quality of the item by 2 if the item's name is 'Aged Brie' and the sellIn date has been reached",
+      function () {
+        let aged_brie = new ItemObj('Aged Brie', 11, 10)
+        const shop = new Shop([aged_brie]);
+        shop.updateQuality()
+        expect(aged_brie.quality).toEqual(11);
+      });
+
+    it("should increase the quality of the item by 1 if the item's name is 'Backstage passes to a TAFKAL80ETC concert' and there are more than 10 sellIn days left",
+      function () {
+        let backstate_passes = new ItemObj('Backstage passes to a TAFKAL80ETC concert', 11, 10)
+        const shop = new Shop([backstate_passes]);
+        shop.updateQuality()
+        expect(backstate_passes.quality).toEqual(11);
+      });
+
+    it("should increase the quality of the item by 2 if the item's name is 'Backstage passes to a TAFKAL80ETC concert' and there are between 5 and 10 sellIn days left",
+      function () {
+        let backstage_passes = new ItemObj('Backstage passes to a TAFKAL80ETC concert', 8, 10)
+        const shop = new Shop([backstage_passes]);
+        shop.updateQuality()
+        expect(backstage_passes.quality).toEqual(12);
+      });
+
+    it("should increase the quality of the item by 3 if the item's name is 'Backstage passes to a TAFKAL80ETC concert' and there are between 1 and 5 sellIn days left",
+      function () {
+        let backstage_passes = new ItemObj('Backstage passes to a TAFKAL80ETC concert', 3, 10)
+        const shop = new Shop([backstage_passes]);
+        shop.updateQuality()
+        expect(backstage_passes.quality).toEqual(13);
+      });
+
+    it("should reduce the quality of the item to 0 if the item's name is 'Backstage passes to a TAFKAL80ETC concert' and sellIn is 0",
+      function () {
+        let backstage_passes = new ItemObj('Backstage passes to a TAFKAL80ETC concert', 0, 10)
+        const shop = new Shop([backstage_passes]);
+        shop.updateQuality()
+        expect(backstage_passes.quality).toEqual(0);
+      });
+
+    it("should degrade in quality by 2 if the item is named 'Conjured' and the sellIn date is above 0",
+      function () {
+        let conjured = new ItemObj('Conjured', 10, 10)
+        const shop = new Shop([conjured]);
+        shop.updateQuality()
+        expect(conjured.quality).toEqual(8);
+      });
+
+    it("should degrade in quality by 4 if the item is named 'Conjured' and the sellIn date is 0 ro below",
+      function () {
+        let conjured = new ItemObj('Conjured', -2, 10)
+        const shop = new Shop([conjured]);
+        shop.updateQuality()
+        expect(conjured.quality).toEqual(6);
       });
 
   })
@@ -92,11 +129,3 @@ describe("Gilded Rose", function () {
   })
 
 });
-
-
-
-
-
-    // statement = new Statement();
-    // spyOn(account.statement, 'addLineToStatement');
-
--- js-jasmine/src/gildedRose.js
+++ js-jasmine/src/gildedRose.js
@@ -5,70 +5,70 @@ class Shop {
 
   updateQuality() {
     this.items.forEach(item => {
+
+      if (item.name === 'Sulfuras, Hand of Ragnaros') return
+
       if (this.checkItemBetween0and50(item.quality)) {
-        if (this.checkItemNotSpecial(item.name)) {
-          item.quality = item.quality - 1
-        } else if (item.name === 'Aged Brie') {
-          item.quality = item.quality + 1
-        } else if (item.name === 'Sulfuras, Hand of Ragnaros') {
-
-        }
-      }
-    })
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-
-      } else {
-        if (this.items[i].quality < 50) {
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+        switch (item.name) {
+          case 'Aged Brie':
+            this.editAgedBrieQuality(item)
+            break;
+          case 'Backstage passes to a TAFKAL80ETC concert':
+            this.editBackStagePassQuality(item)
+            break;
+          case 'Conjured':
+            this.editConjuredQuality(item)
+            break;
+          default:
+            this.editStandardItemQuality(item)
         }
       }
     }
-
-    // return this.items;
-  }
-
-  checkItemNotSpecial(name) {
-    return (name !== 'Aged Brie' && name !== 'Backstage passes to a TAFKAL80ETC concert'
-      && name !== 'Sulfuras, Hand of Ragnaros') ? true : false
+    )
   }
 
   checkItemBetween0and50(quality) {
-    console.log(quality)
     return (quality > 0 && quality < 50) ? true : false
   }
 
+  editStandardItemQuality(item) {
+    if (item.sellIn > 0) {
+      item.quality -= 1
+    } else {
+      item.quality -= 2
+    }
+  }
 
+  editAgedBrieQuality(item) {
+    if (item.sellIn > 0) {
+      item.quality += 1
+    } else {
+      item.quality += 2
+    }
+  }
 
+  editBackStagePassQuality(item) {
+    if (item.sellIn <= 10 && item.sellIn > 5) {
+      item.quality += 2
+    } else if (item.sellIn < 5 && item.sellIn > 0) {
+      item.quality += 3
+    } else if (item.sellIn === 0) {
+      item.quality = 0
+    } else {
+      item.quality += 1
+    }
+  }
+
+  editConjuredQuality(item) {
+    if (item.sellIn > 0) {
+      item.quality -= 2
+    } else {
+      item.quality -= 4
+    }
+  }
 }
+
+
+
+
+
